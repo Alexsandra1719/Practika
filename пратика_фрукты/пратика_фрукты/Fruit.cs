31febe5faada9f5fea82f3c18706ddc49d01202d
Alexandra Shcherbina <Shcherbina.1719@gmail.com>
2025-07-10 16:13:37 +0300

    Добавила два класса и определила своиства. Подготовила дизайн формы к проекту

--- пратика_фрукты/пратика_фрукты/Fruit.cs
+++ пратика_фрукты/пратика_фрукты/Fruit.cs
@@ -3,5 +3,16 @@ using Newtonsoft.Json;
 
 public class Fruit
 {
-    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("family")]
+    public string Family { get; set; }
+
+    [JsonProperty("genus")]
+    public string Genus { get; set; }
+
+    [JsonProperty("nutritions")]
+    public Nutritions Nutritions { get; set; }
+
 }
--- пратика_фрукты/пратика_фрукты/Nutritions.cs
+++ пратика_фрукты/пратика_фрукты/Nutritions.cs
@@ -2,5 +2,19 @@
 
 public class Nutritions
 {
-   
+    [JsonProperty("calories")]
+    public decimal Calories { get; set; }
+
+    [JsonProperty("fat")]
+    public decimal Fat { get; set; }
+
+    [JsonProperty("sugar")]
+    public decimal Sugar { get; set; }
+
+    [JsonProperty("carbohydrates")]
+    public decimal Carbohydrates { get; set; }
+
+    [JsonProperty("protein")]
+    public decimal Protein { get; set; }
+
 }
